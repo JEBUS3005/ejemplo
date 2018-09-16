2ce75b9aea0f477fc652091b207ea9106f8b6c64
Jebus <Jebus@J-Laptop>
2018-09-16 01:31:17 -0400

    creacion de clase

---
+++ src/hola/MiClase.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hola;
+
+/**
+ *
+ * @author Jebus
+ */
+public class MiClase {
+    int n;
+
+    public int getN() {
+        return n;
+    }
+
+    public void setN(int n) {
+        this.n = n;
+    }
+    
+    
+    
+}
